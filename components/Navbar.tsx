cd654a72bc66fb45711fb00d50ee86168f32ce9f
Orway <drkhn.dk@gmail.com>
2025-06-03 01:27:58 +0300

    v0.1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,34 @@
 // components/Navbar.tsx
 "use client";
 
+import "./Navbar.css";
+
 import Link from "next/link";
+import Image from "next/image";
+
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ['700'], // ihtiyacına göre ayarla
+    variable: '--font-poppins', // CSS değişkeni tanımlarsan globalde kullanırsın
+});
+
 
 export default function Navbar() {
     return (
         <nav className="bg-black-80 shadow py-4 px-8 flex justify-between">
-            <span className="font-bold">Dorukhan KILIÇASLAN</span>
-            <div className="space-x-4">
+            <span className="logoBox">
+                <Image
+                    src="https://res.cloudinary.com/ds4suhwnb/image/upload/v1748902508/DK_LogoWhite_cgyafw.png"
+                    alt="Logo"
+                    width={50}
+                    height={50}
+                    className="logoImage"
+                />
+                <span className="logoText">Dorukhan KILIÇASLAN</span>
+            </span>
+            <div className="space-x-4 flex items-center">
                 <Link href="/" className="text-white"><span className="text-l">Anasayfa</span></Link>
                 <Link href="/projects" className="text-white"><span className="text-l">Projeler</span></Link>
                 <Link href="/about" className="text-white"><span className="text-l">Hakkımda</span></Link>
