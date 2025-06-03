509945360952814db3f39b57451847cdb26fcb8d
Orway <drkhn.dk@gmail.com>
2025-06-04 02:11:26 +0300

    fixedVercelErrors

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
       <main>
         <h1 className="font-bold mainPageMainText">
           <p>Merhaba, ben Dorukhan KILIÇASLAN.</p>
-          <p>Ben İstanbul'da yaşayan bir grafik tasarımcısıyım.</p>
+          <p>Ben İstanbul&#39;da yaşayan bir grafik tasarımcısıyım.</p>
           <p>Marka, kullanıcı arayüzü (UI) tasarımı, <span className="_3dText">3D</span> Modelleme üzerine odaklanıyorum.</p>
         </h1>
       </main>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 // components/Footer.tsx
 
-import Link from "next/link";
+//import Link from "next/link";
 import "./Footer.css";
 
 export default function Footer() {
